dfb43c95725c62d2fd7b956b21c90a86a7c60804
ensabanur <ryan@beersonthebalcony.com>
2017-06-06 12:41:20 +1000

    Create a New Nic and Add to a VM

---
+++ AddNewNictoVM.ps1
@@ -0,0 +1,43 @@
+### 	Add New Nic to Azure RM VM 		###
+###						###
+###	This will REBOOT your vm		###
+###						###
+###################################################
+
+#Variables
+$VMname = "MHAZFGTFWTEST"
+$VMRG =  "MHAZFGTFWTESTRG"
+$NICName = "MHAZFGTFWTESTNic2"
+$NICRG = "MHAZFGTFWTESTRG"
+$VNetName = "MHAZEXPVNET01"
+$subnetname = "MHAZEXPSUB01"
+$VnetRG = "MHAZSYDGENRG"
+$Location = "Australia East"
+
+#Get the subnet ID	
+	$Subnets = Get-AzureRmVirtualNetwork -Name "$($VnetName)" -ResourceGroupName "$($VNetRG)" | Get-AzureRmVirtualNetworkSubnetConfig
+	$subnetID = $subnets.id | ?{$_ -like "*$($subnetname)*"}
+
+#Create the NIC
+	$NewNic = New-AzureRmNetworkInterface -name "$($NicName)" -ResourceGroupName "$($NicRG)" -Location "$($Location)" -SubnetId "$($SubnetID)"
+
+#Get the VM
+	$VM = Get-AzureRmVM -Name $VMname -ResourceGroupName $VMRG
+
+#Add the second NIC
+	$VM = Add-AzureRmVMNetworkInterface -VM $VM -Id $NewNIC.Id
+
+# Show the Network interfaces so we can see secondary in the config
+	$VM.NetworkProfile.NetworkInterfaces
+
+#Set the original Nic as primary	
+	$VM.NetworkProfile.NetworkInterfaces[0].Primary = $true
+
+#Stop the VM
+	$VM | Stop-AzureRMVM -force -confirm:$false
+
+#Update the VM configuration (The VM will be restarted)
+Update-AzureRmVM -VM $VM -ResourceGroupName $VMRG
+
+#Start the VM
+	$VM | Start-AzureRMVM
